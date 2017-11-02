e36946050b8923ca287af87cce4fef3964a4cab8
Metodi Sabev <metodi.sabev@techhuddle.com>
2017-11-02 17:14:48 +0200

    Add variables check

--- bin/install_awscli.sh
+++ bin/install_awscli.sh
@@ -6,19 +6,23 @@ chmod u+x $INSTALL_DIR/bin/aws
 
 export PATH=~/vendor/awscli/bin:$PATH
 
-# if [ ! -d ~/.aws ]; then
+if [ ! -d ~/.aws ]; then
 
 mkdir ~/.aws
 
+if [ -n "$AWS_KEY" ]; then
 cat << EOF >> ~/.aws/credentials
 [default]
 aws_access_key_id = $AWS_KEY
 aws_secret_access_key = $AWS_SECRET_KEY
 EOF
+fi
 
+if [ -n "$AWS_REGION" ]; then
 cat << EOF >> ~/.aws/config 
 [default]
 region = $AWS_REGION
 EOF
+fi
 
-# fi
+fi

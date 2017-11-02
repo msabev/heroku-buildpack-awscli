5d1c1c52704a3299624e1392482215d30d33fbfc
Metodi Sabev <metodi.sabev@techhuddle.com>
2017-11-02 16:42:36 +0200

    Update install script

--- bin/install_awscli.sh
+++ bin/install_awscli.sh
@@ -4,15 +4,19 @@ chmod +x /app/vendor/awscli-bundle/install
 /app/vendor/awscli-bundle/install -i $INSTALL_DIR
 chmod u+x $INSTALL_DIR/bin/aws
 
-mkdir ~/.aws
+export PATH=~/vendor/awscli/bin:$PATH
 
-cat >> ~/.aws/credentials << EOF
-[default]
-aws_access_key_id = $AWS_KEY
-aws_secret_access_key = $AWS_SECRET_KEY
-EOF
+if [ ! -d ".aws" ]; then
+    mkdir ~/.aws
 
-cat >> ~/.aws/config << EOF 
-[default]
-region = $AWS_REGION
-EOF
+    cat >> ~/.aws/credentials << EOF
+    [default]
+    aws_access_key_id = $AWS_KEY
+    aws_secret_access_key = $AWS_SECRET_KEY
+    EOF
+
+    cat >> ~/.aws/config << EOF 
+    [default]
+    region = $AWS_REGION
+    EOF
+fi
